a35ee2251253d9406be353eec20b30d6355bbe0f
ZeroT <111341000+PhamTuanKiet002@users.noreply.github.com>
2022-11-09 17:47:50 +0700

    code

---
+++ routes/index.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose'); 
+
+mongoose.connect('mongodb+srv://Khoa21donga:Khoa21dongaKhoa21dongaKhoa21donga@cluster0.o9ivn6p.mongodb.net/test2?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+let Custommer4Schema = mongoose.Schema({
+  IdDistrict: {
+    type: String,
+  },
+  IdVillage: {
+    type: String,
+  },
+  Address: {
+    type: String,
+  },
+  BankName: {
+    type: String,
+  },
+  BranchName: {
+    type: String,
+  }
+});
+let Custommer4 = mongoose.model('Custommer4', Custommer4Schema);
+
+router.get('/', function (req, res, next) {
+  Custommer4.find({}, (Error, data) => {
+    res.render('index', { Custommer4s: data });
+  });
+});
+
+router.get('/form-add', function (req, res, next) {
+  res.render('form-add', {});
+});
+
+router.post('/add', function (req, res, next) {
+  Custommer4.create(req.body);
+  res.redirect('/');
+});
+
+router.get('/form-update/:id', function (req, res, next) {
+  Custommer4.findById(req.params.id, (Error, data) => {
+    res.render('form-update', { Custommer4s: data });
+  });
+});
+
+router.post('/update', function (req, res, next) {
+  Custommer4.findByIdAndUpdate(req.body.id, req.body, (Error, data) => {
+    res.redirect('/');
+  });
+})
+
+router.get('/form-delete/:id', function (req, res, next) {
+  Custommer4.findByIdAndDelete(req.params.id, (Error, data) => {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
